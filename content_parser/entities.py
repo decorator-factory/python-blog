333bd7aeb2fa2a9d3f564aa2c0f5131062b584de
decorator-factory <42166884+decorator-factory@users.noreply.github.com>
2020-10-18 04:38:17 +0300

    implement basic entities

---
+++ content_parser/entities.py
@@ -0,0 +1,116 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from typing import Callable, Dict, Tuple
+from content_parser import entity_types as et
+import html
+
+
+class Entity:
+    @property
+    def ty(self):
+        return et.TAny
+
+    def render(self) -> str:
+        if hasattr(self, "render_inline"):
+            return self.render_inline() # type: ignore
+        if hasattr(self, "render_block"):
+            return self.render_block() # type: ignore
+        raise TypeError(f"Cannot render {self}")
+
+
+@dataclass(frozen=True, eq=True)
+class Integer(Entity):
+    value: int
+
+    ty = et.TInt()
+
+    def render_inline(self):
+        return str(self.value)
+
+
+@dataclass(frozen=True, eq=True)
+class String(Entity):
+    value: str
+
+    ty = et.TStr()
+
+    def render_inline(self):
+        return html.escape(self.value, quote=True)
+
+
+@dataclass(frozen=True, eq=True)
+class InlineTag(Entity):
+    tag: str
+    options: str
+    children: Tuple[Entity, ...]
+
+    ty = et.TInline()
+
+    def render_inline(self):
+        return (
+             f"<{self.tag} {self.options}>"
+            + "".join(e.render_inline() for e in self.children) # type: ignore
+            +f"</{self.tag}>"
+        )
+
+
+@dataclass(frozen=True, eq=True)
+class BlockTag(Entity):
+    tag: str
+    options: str
+    children: Tuple[Entity, ...]
+
+    ty = et.TBlock()
+
+    def render_block(self):
+        return (
+             f"<{self.tag} {self.options}>"
+            + "".join(e.render() for e in self.children)
+            +f"</{self.tag}>"
+        )
+
+
+@dataclass(frozen=True, eq=True)
+class Function(Entity):
+    overloads: Dict[et.TFunction, Callable]
+
+    @property
+    def ty(self):
+        return et.TUnion(tuple(self.overloads.keys()))
+
+    def call(self, *args):
+        for (o, f) in self.overloads.items():
+            if o.rest_type is None:
+                same_length = len(o.arg_types) == len(args)
+                types_match = all(
+                    expected_type.match(arg)
+                    for arg, expected_type in zip(args, o.arg_types)
+                )
+                if same_length and types_match:
+                    return f(*args)
+            else:
+                for i in range(len(args) + 1):
+                    positional_args, rest_args = args[:i], args[i:]
+
+                    # check if the rest_args are the same
+                    assert o.rest_type is not None
+                    if not all(o.rest_type.match(r) for r in rest_args):
+                        break
+
+                    for arg, expected_type in zip(positional_args, o.arg_types):
+                        if not expected_type.match(arg):
+                            break
+
+                    return f(*positional_args, *rest_args)
+        raise TypeError(f"Cannot call {self} with {args}")
+
+
+    def return_type_when_called_with(self, *, args, rest):
+        # TODO: implement these cases:
+        # (λ x ...y . z) can be called as (λ x y y y . z)
+        # (λ x y ...y . z) can be called as (λ x y y y . z)
+        # (λ x y y y y ...y . z) can't be called as (λ x y y y . z)
+        for o in self.overloads.keys():
+            if o.arg_types == args and o.rest_type == rest:
+                return o.return_type
+        return None
---
+++ content_parser/tests/test_entities.py
@@ -0,0 +1,48 @@
+import content_parser.entity_types as et
+import content_parser.entities as e
+
+
+def test_function_with_fixed_signature():
+    add = e.Function({
+        et.TFunction(
+            (et.TInt(), et.TInt()),
+            None,
+            et.TInt()
+        ): lambda x, y: e.Integer(x.value + y.value)
+    })
+
+    assert add.call(e.Integer(5), e.Integer(4)) == e.Integer(9)
+
+
+def test_function_with_varargs_signature():
+    add = e.Function({
+        et.TFunction(
+            (),
+            et.TInt(),
+            et.TInt()
+        ): lambda *args: e.Integer(sum(e.value for e in args))
+    })
+
+    assert add.call() == e.Integer(0)
+    assert add.call(e.Integer(5)) == e.Integer(5)
+    assert add.call(e.Integer(5), e.Integer(4)) == e.Integer(9)
+    assert add.call(e.Integer(5), e.Integer(4), e.Integer(1)) == e.Integer(10)
+
+
+def test_overloaded_function():
+    add = e.Function({
+        et.TFunction(
+            (et.TInt(), et.TInt()),
+            None,
+            et.TInt()
+        ): lambda x, y: e.Integer(x.value + y.value),
+
+        et.TFunction(
+            (et.TStr(), et.TStr()),
+            None,
+            et.TStr()
+        ): lambda x, y: e.String(x.value + y.value)
+    })
+
+    assert add.call(e.Integer(5), e.Integer(4)) == e.Integer(9)
+    assert add.call(e.String("ab"), e.String("cd")) == e.String("abcd")
